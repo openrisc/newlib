770b0938e47213470cb69d3da14624c8d515c354
Stefan Wallentowitz <stefan@wallentowitz.de>
2016-03-13 11:12:11 +0100

    Extend script
    Checkout repositories, proper logging and add gdb

--- build.sh
+++ build.sh
@@ -22,39 +22,60 @@ fi
 
 # Location of newlib repository
 if [ "x$NEWLIB_CHECKOUT" == "x" ]; then
-    git clone git://sourceware.org/git/newlib-cygwin.git newlib
+    if [ ! -d newlib ]; then
+	git clone git://sourceware.org/git/newlib-cygwin.git newlib
+    fi
     export NEWLIB_CHECKOUT=$PWD/newlib
 fi
 
-# Clone binutils & GCC repositories
-git clone git://sourceware.org/git/binutils-gdb.git
-git clone https://github.com/openrisc/or1k-gcc.git gcc
+# Clone binutils and gdb if necessary
+if [ ! -d binutils ]; then
+    git clone git://sourceware.org/git/binutils-gdb.git binutils
+fi
+
+if [ ! -d gdb ]; then
+    git clone https://github.com/openrisc/binutils-gdb.git gdb
+fi
+
+# Clone gcc if necessary
+if [ ! -d gcc ]; then
+    git clone https://github.com/openrisc/or1k-gcc.git gcc
+fi
+
+rm -rf build-*
 
 # Build!
 echo "++ build binutils"
 mkdir build-binutils; cd build-binutils
-../binutils-gdb/configure --target=or1k-elf --prefix=$PREFIX --enable-shared --disable-itcl --disable-tk --disable-tcl --disable-winsup --disable-gdbtk --disable-libgui --disable-rda --disable-sid --disable-sim --disable-gdb --with-sysroot --disable-newlib --disable-libgloss --with-system-zlib > $LOG
-make > $LOG
-make install > $LOG
+../binutils/configure --target=or1k-elf --prefix=$PREFIX --enable-shared --disable-itcl --disable-tk --disable-tcl --disable-winsup --disable-gdbtk --disable-libgui --disable-rda --disable-sid --disable-sim --disable-gdb --with-sysroot --disable-newlib --disable-libgloss --with-system-zlib &> $LOG
+make &> $LOG
+make install &> $LOG
 cd ..
 
 echo "++ build gcc (stage 1)"
 mkdir build-gcc-stage1; cd build-gcc-stage1
-../gcc/configure --target=or1k-elf --prefix=$PREFIX --enable-languages=c --disable-shared --disable-libssp > $LOG
-make > $LOG
-make install > $LOG
+../gcc/configure --target=or1k-elf --prefix=$PREFIX --enable-languages=c --disable-shared --disable-libssp &> $LOG
+make &> $LOG
+make install &> $LOG
 cd ..
 
 echo "++ build newlib"
 mkdir build-newlib; cd build-newlib
-${NEWLIB_CHECKOUT}/configure --target=or1k-elf --prefix=$PREFIX ${CFLAGS_PARAM} > $LOG
-make > $LOG
-make install > $LOG
+${NEWLIB_CHECKOUT}/configure --target=or1k-elf --prefix=$PREFIX ${CFLAGS_PARAM} &> $LOG
+make &> $LOG
+make install &> $LOG
 cd ..
 
 echo "++ build gcc (stage 2)"
 mkdir build-gcc-stage2; cd build-gcc-stage2
-../gcc/configure --target=or1k-elf --prefix=$PREFIX --enable-languages=c,c++ --disable-shared --disable-libssp --with-newlib > $LOG
-make > $LOG
-make install > $LOG
+../gcc/configure --target=or1k-elf --prefix=$PREFIX --enable-languages=c,c++ --disable-shared --disable-libssp --with-newlib &> $LOG
+make &> $LOG
+make install &> $LOG
+cd ..
+
+echo "++ build gdb"
+mkdir build-gdb; cd build-gdb
+../gdb/configure --target=or1k-elf --prefix=$PREFIX --enable-shared --disable-itcl --disable-tk --disable-tcl --disable-winsup --disable-gdbtk --disable-libgui --disable-rda --disable-sid --disable-sim --disable-or1ksim --with-sysroot --disable-newlib --disable-libgloss --disable-gas --disable-ld --disable-binutils --disable-gprof --with-system-zlib &> $LOG
+make &> $LOG
+make install &> $LOG
 cd ..

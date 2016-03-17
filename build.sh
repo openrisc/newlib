0d33571b3423bd8ce1c8eca6a37cd3179bfed708
Stefan Wallentowitz <stefan@wallentowitz.de>
2016-03-17 22:08:38 +0100

    Handle multiple jobs

--- build.sh
+++ build.sh
@@ -23,6 +23,10 @@ if [ "x$NEWLIB_CHECKOUT" == "x" ]; then
     export NEWLIB_CHECKOUT=$PWD/newlib
 fi
 
+if [ "x$JOBS" != "x" ]; then
+    export JOBS="-j $JOBS"
+fi
+
 # Clone binutils and gdb if necessary
 if [ ! -d binutils ]; then
     git clone git://sourceware.org/git/binutils-gdb.git binutils
@@ -47,34 +51,34 @@ fi
 echo "++ build binutils"
 mkdir build-binutils; cd build-binutils
 eval ../binutils/configure --target=or1k-elf --prefix=$PREFIX --disable-itcl --disable-tk --disable-tcl --disable-winsup --disable-gdbtk --disable-libgui --disable-rda --disable-sid --disable-sim --disable-gdb --with-sysroot --disable-newlib --disable-libgloss --with-system-zlib $LOG
-eval make $LOG
+eval make $JOBS $LOG
 eval make install $LOG
 cd ..
 
 echo "++ build gcc (stage 1)"
 mkdir build-gcc-stage1; cd build-gcc-stage1
 eval ../gcc/configure --target=or1k-elf --prefix=$PREFIX --enable-languages=c --disable-shared --disable-libssp $LOG
-eval make $LOG
+eval make $JOBS $LOG
 eval make install $LOG
 cd ..
 
 echo "++ build newlib"
 mkdir build-newlib; cd build-newlib
 eval ${NEWLIB_CHECKOUT}/configure --target=or1k-elf --prefix=$PREFIX ${CFLAGS_PARAM} $LOG
-eval make $LOG
+eval make $JOBS $LOG
 eval make install $LOG
 cd ..
 
 echo "++ build gcc (stage 2)"
 mkdir build-gcc-stage2; cd build-gcc-stage2
 eval ../gcc/configure --target=or1k-elf --prefix=$PREFIX --enable-languages=c,c++ --disable-shared --disable-libssp --with-newlib $LOG
-eval make $LOG
+eval make $JOBS $LOG
 eval make install $LOG
 cd ..
 
 echo "++ build gdb"
 mkdir build-gdb; cd build-gdb
 eval ../gdb/configure --target=or1k-elf --prefix=$PREFIX --enable-shared --disable-itcl --disable-tk --disable-tcl --disable-winsup --disable-gdbtk --disable-libgui --disable-rda --disable-sid --disable-sim --disable-or1ksim --with-sysroot --disable-newlib --disable-libgloss --disable-gas --disable-ld --disable-binutils --disable-gprof --with-system-zlib $LOG
-eval make $LOG
+eval make $JOBS $LOG
 eval make install $LOG
 cd ..

1fc5f3432d99e5d88fb128b9fb3457da4fecf435
Stefan Wallentowitz <stefan@wallentowitz.de>
2016-03-17 17:55:35 +0100

    Add log redirecting of stdout
    Without any log, travis does not complete, so only seeing stderr
    should be sufficient.

--- build.sh
+++ build.sh
@@ -39,38 +39,42 @@ fi
 
 rm -rf build-*
 
+if [ "x$LOG_REDIRECT" != "x" ]; then
+    export LOG='> $LOG_REDIRECT'
+fi
+
 # Build!
 echo "++ build binutils"
 mkdir build-binutils; cd build-binutils
-../binutils/configure --target=or1k-elf --prefix=$PREFIX --disable-itcl --disable-tk --disable-tcl --disable-winsup --disable-gdbtk --disable-libgui --disable-rda --disable-sid --disable-sim --disable-gdb --with-sysroot --disable-newlib --disable-libgloss --with-system-zlib
-make
-make install
+eval ../binutils/configure --target=or1k-elf --prefix=$PREFIX --disable-itcl --disable-tk --disable-tcl --disable-winsup --disable-gdbtk --disable-libgui --disable-rda --disable-sid --disable-sim --disable-gdb --with-sysroot --disable-newlib --disable-libgloss --with-system-zlib $LOG
+eval make $LOG
+eval make install $LOG
 cd ..
 
 echo "++ build gcc (stage 1)"
 mkdir build-gcc-stage1; cd build-gcc-stage1
-../gcc/configure --target=or1k-elf --prefix=$PREFIX --enable-languages=c --disable-shared --disable-libssp
-make
-make install
+eval ../gcc/configure --target=or1k-elf --prefix=$PREFIX --enable-languages=c --disable-shared --disable-libssp $LOG
+eval make $LOG
+eval make install $LOG
 cd ..
 
 echo "++ build newlib"
 mkdir build-newlib; cd build-newlib
-${NEWLIB_CHECKOUT}/configure --target=or1k-elf --prefix=$PREFIX ${CFLAGS_PARAM}
-make
-make install
+eval ${NEWLIB_CHECKOUT}/configure --target=or1k-elf --prefix=$PREFIX ${CFLAGS_PARAM} $LOG
+eval make $LOG
+eval make install $LOG
 cd ..
 
 echo "++ build gcc (stage 2)"
 mkdir build-gcc-stage2; cd build-gcc-stage2
-../gcc/configure --target=or1k-elf --prefix=$PREFIX --enable-languages=c,c++ --disable-shared --disable-libssp --with-newlib
-make
-make install
+eval ../gcc/configure --target=or1k-elf --prefix=$PREFIX --enable-languages=c,c++ --disable-shared --disable-libssp --with-newlib $LOG
+eval make $LOG
+eval make install $LOG
 cd ..
 
 echo "++ build gdb"
 mkdir build-gdb; cd build-gdb
-../gdb/configure --target=or1k-elf --prefix=$PREFIX --enable-shared --disable-itcl --disable-tk --disable-tcl --disable-winsup --disable-gdbtk --disable-libgui --disable-rda --disable-sid --disable-sim --disable-or1ksim --with-sysroot --disable-newlib --disable-libgloss --disable-gas --disable-ld --disable-binutils --disable-gprof --with-system-zlib
-make
-make install
+eval ../gdb/configure --target=or1k-elf --prefix=$PREFIX --enable-shared --disable-itcl --disable-tk --disable-tcl --disable-winsup --disable-gdbtk --disable-libgui --disable-rda --disable-sid --disable-sim --disable-or1ksim --with-sysroot --disable-newlib --disable-libgloss --disable-gas --disable-ld --disable-binutils --disable-gprof --with-system-zlib $LOG
+eval make $LOG
+eval make install $LOG
 cd ..
